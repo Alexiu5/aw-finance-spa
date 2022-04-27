75bfabb437e6ef2df48b083f8f45ebc65fe48d01
Alex <alexius9000@gmail.com>
2022-04-27 17:05:10 -0500

    framer-motion added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
 import "./App.sass";
+import { m, motion, Variants } from "framer-motion";
 
 function App() {
-  return <div className="App">Sup fella</div>;
+  const cardVariants: Variants = {
+    offscreen: {
+      y: 300,
+    },
+    onscreen: {
+      y: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8,
+      },
+    },
+  };
+  return (
+    <div className="App">
+      <motion.ul
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+      >
+        <motion.li className="card-item" variants={cardVariants}>
+          card-item
+        </motion.li>
+        <motion.li className="card-item" variants={cardVariants}>
+          card-item
+        </motion.li>
+        <motion.li className="card-item" variants={cardVariants}>
+          card-item
+        </motion.li>
+        <motion.li className="card-item" variants={cardVariants}>
+          card-item
+        </motion.li>
+        <motion.li className="card-item" variants={cardVariants}>
+          card-item
+        </motion.li>
+        <motion.li className="card-item" variants={cardVariants}>
+          card-item
+        </motion.li>
+      </motion.ul>
+    </div>
+  );
 }
 
 export default App;
